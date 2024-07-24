c3dd003560f2bfa5d045e0b7d4748889be4f666a
Dani <dani@MacBook.local>
2024-07-24 16:32:33 -0300

    Primer Commit

---
+++ src/app/components/home/Hero/Hero.tsx
@@ -0,0 +1,9 @@
+import styles from './Hero.module.sass'
+export const Hero = () => {
+  return (
+    <section className={styles.Hero}>
+      <h1>Beras</h1>
+      <h2>Tu marca de confianza</h2>
+    </section>
+  )
+}
