5ef554a336f14a11d0124f7e2d54a2993ad70869
Dani <dani@MacBook.local>
2024-07-29 16:38:56 -0300

    Segundo Commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,7 @@
 import { fileURLToPath } from 'url';
 import { dirname, join } from 'path';
 
-/** @type {import('next').NextConfig} */
+/* @type {import('next').NextConfig} */
 
 // Get the directory name of the current module
 const __filename = fileURLToPath(import.meta.url);
@@ -12,6 +12,12 @@ const nextConfig = {
     includePaths: [join(__dirname, 'src/sass')],
     prependData: `@import "main.sass"`,
   },
+  images: {
+    remotePatterns: [{
+      hostname: "cdn.shopify.com",
+      protocol: 'https',
+    }],
+  }
 };
 
 export default nextConfig;
