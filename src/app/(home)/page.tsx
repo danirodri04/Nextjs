fdb5a638af91dc694e183fc024d3b1de56f72409
Dani <dani@MacBook.local>
2024-07-30 16:10:15 -0300

    Tercer commit

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -3,7 +3,7 @@ import { MainProducts } from "../components/home/MainProducts";
 export default function Home() {
   return (
     <main>
-      <MainProducts />
+      <MainProducts /> 
     </main>
   );
 }
--- src/app/(home)/template.tsx
+++ src/app/(home)/template.tsx
@@ -1,7 +1,15 @@
+"use client"
+
 import { Hero } from "../components/home/Hero"
 import { Description } from "../components/home/Description"
+import { useEffect } from "react"
 
 export default function HomeLayout({ children }: { children: React.ReactNode }) {
+  
+  useEffect(() => {
+    console.log('Envio de metricas')
+  }, [])
+
   return (
     <div>
       <Hero />
